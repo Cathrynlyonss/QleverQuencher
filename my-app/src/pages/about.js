75008dc884b1b1802b615aa09f4f3052ff8fa2e8
kianaerickson <kiana-erickson@uiowa.edu>
2023-04-10 14:00:19 -0500

    Water image on about page

--- my-app/src/pages/about.js
+++ my-app/src/pages/about.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import backgroundImg from '../img/splash1.jpeg'
 
 class about extends React.Component {
 
     render() {
         return (
-            <div className='Page-cover'>
+            <div style={{ backgroundImage: `url(${backgroundImg})`,
+            backgroundSize: 'cover',
+            backgroundPosition: ' 100 00',
+            backgroundRepeat: 'no-repeat',
+            height: '100vh',
+            width: '100vw', 
+            }} className='Page-cover'>
                 <Box sx={{p:5}}>
                     <Typography variant="h2" component="div" gutterBottom align='center'>
                         About QueverQuencher
+                    </Typography>
+                    <Typography variant="subtitle1" component="div" gutterBottom align='center'>
+                        QueverQuencher is a web application that allows users to track their water consumption and meet their daily water intake goals.
                     </Typography>
                 </Box>
             </div>
