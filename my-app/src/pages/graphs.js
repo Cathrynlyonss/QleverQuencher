4c064f5ec5a9d49b9a92ff2588df126c42563d66
kianaerickson <kiana-erickson@uiowa.edu>
2023-04-10 14:07:13 -0500

    Background color on graphs page

--- my-app/src/pages/graphs.js
+++ my-app/src/pages/graphs.js
@@ -2,14 +2,21 @@ import React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
+
+
+
 class graphs extends React.Component {
 
     render() {
         return (
-            <div className='Page-cover'>
+            <div style = {{
+                backgroundColor: '#DAEDFF',
+                width: '100vw',
+                height: '100vh'
+            }}>
                 <Box sx={{p:5}}>
                     <Typography variant="h2" component="div" gutterBottom align='center'>
-                        Graphs
+                            Graphs
                     </Typography>
                 </Box>
             </div>
