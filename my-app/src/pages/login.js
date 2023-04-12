1e0d95d8c3539eab7336c29b87839664a1ed0ddb
kianaerickson <kiana-erickson@uiowa.edu>
2023-04-11 19:01:04 -0500

    Passcode visibility button

--- my-app/src/pages/login.js
+++ my-app/src/pages/login.js
@@ -6,6 +6,13 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const theme = createTheme({
   palette: {
@@ -19,6 +26,14 @@ export default function Login() {
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
 
     const handleFormSubmit = (event) => {
         console.log("Form submitted");
@@ -47,7 +62,29 @@ export default function Login() {
                         Login
                     </Typography>
                     <TextField sx={{mb: '20px'}} type='email' label="Email" variant="outlined" value={email} onChange={(e) => setEmail(e.target.value)}/>
-                    <TextField sx={{mb: '20px'}} type='password' label="Password" variant="outlined" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <FormControl sx={{ m: 1, width: '60ch' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            gutterBottom align='center'
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            endAdornment={
+              <InputAdornment>
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
                     <Button 
                         variant="contained"
                         type='submit'
